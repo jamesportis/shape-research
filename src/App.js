f7ba495bc1a5ba2f939d7b59becf5d2ac459ce53
jamesportis <jamesportis@yahoo.com>
2019-04-26 19:19:36 -0700

    created shape research

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Exercise from "./components/exercise";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Exercise />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/exercise.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Exercise extends Component {
+  render() {
+    return (
+      <div className="Exercise">
+        <p> Exercise </p>
+        <p>test</p>
+        <p>more bad code</p>
+      </div>
+    );
+  }
+}
+
+export default Exercise;
